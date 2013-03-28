0d2ed97ab201eaaf3ad05cff59da8e48f0e7b848
Veljko Dragsic <veljko@kset.org>
2013-03-28 21:40:39 +0100

    Basic steal setup

---
+++ app/app.js
@@ -0,0 +1,18 @@
+// Load all of the plugin dependencies
+steal(
+	//'./styles.css',
+	'jquery',
+	'can',
+	function(){
+		
+		// Create the state that will be shared by everything
+		var currentState = can.compute();
+
+		// Controllers
+		// init
+		
+		// 
+		console.log("BitHub client");
+	});
+
+
