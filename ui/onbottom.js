281104aa60c984dcbb4faa905bcc7c11ff920b7d
Veljko Dragsic <veljko@kset.org>
2013-04-01 19:03:42 +0200

    OnBottom control added.

---
+++ ui/onbottom.js
@@ -0,0 +1,29 @@
+steal(
+	'can',
+	function () {
+		return can.Control({
+			defaults: { 
+				treshold: 50,
+				onbottom: false
+			}
+		}, {
+			init: function(element, options) {
+				this.options.treshold = options.treshold || this.options.treshold;
+			},
+
+			'scroll': function (element) {
+				var self = this,
+					verticalPositon = self.options.verticalPositon;
+
+				verticalPositon = self.element.height() - self.element.scrollTop() - self.options.treshold;
+				if (( verticalPositon <= window.innerHeight) && self.options.onbottom == false) {
+					self.options.onbottom = true;
+					self.element.trigger('onbottom');
+				};
+				if (( verticalPositon > window.innerHeight) && self.options.onbottom == true) {
+					self.options.onbottom = false;
+				};
+			}
+			
+		});
+	});
