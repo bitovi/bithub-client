ad6bc460acb1ec2c67ecdb790f367f1e855b23a9
Veljko Dragsic <veljko@kset.org>
2013-04-03 17:37:03 +0200

    categories and projects methods on Tag model

--- app/models/tags.js
+++ app/models/tags.js
@@ -1,10 +1,35 @@
 steal('can',
 	  function (can) {
 		  return can.Model({
+
+			  // CRUD
 			  findAll : 'GET /api/tags',
 			  findOne : 'GET /api/tags/{id}',
 			  create  : 'POST /api/tags',
 			  update  : 'PUT /api/tags/{id}',
-			  destroy : 'DELETE /api/tags/{id}'
+			  destroy : 'DELETE /api/tags/{id}',
+
+			  categories: function (params, success, error) {
+				  can.ajax({
+					  url: '/api/tags/categories',
+					  type: 'GET',
+					  async: true,
+					  dataType: 'json',
+					  success: function(data) { success(data['data']); },
+					  error: error
+				  });
+			  },
+
+			  projects: function (params, success, error) {
+				  can.ajax({
+					  url: '/api/tags/projects',
+					  type: 'GET',
+					  async: true,
+					  dataType: 'json',
+					  success: function(data) { success(data['data']); },
+					  error: error
+				  });
+			  }
+
 		  }, {});
 	  });
