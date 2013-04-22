931e07676629f638e5c28f7c71ef36cbd5907c15
Veljko Dragsic <veljko@kset.org>
2013-04-22 22:23:30 +0200

    GroupByArray enhanced, handles nested keys like 'foo.bar.baz'

--- bithub/helpers/group.js
+++ bithub/helpers/group.js
@@ -1,25 +1,17 @@
 steal(
 	'jquery',
 	function () {
-
-		var groupIntoArrayIter = function (result, keys, value) {
-			// pop out first (destructive!)
-			var key = keys.splice(0,1);
-
-			// do nesting and finally push object
-			if (key.length > 0) {
-
-				// find or create nesting object
-				if (result.filter(function (item) { if (item.key == value[key]) return item; }).length == 0) {
-					result.push({key: value[key], value: []});
-				}
-
-				// do iter
-				groupIntoArrayIter(result[result.length-1].value, keys, value);
-				
-			} else {
-				result.push(value);
-			};
+		
+		/*
+		 * Splits selector by '.' into keys, then transverse object and returns matched value
+		 * 
+		 * Example:
+		 *   obj = { foo: { bar: { baz: "123" }}};
+		 *   trans(obj, 'foo.bar.baz') // returns "123"
+		 */		
+		var transverseByKey = function(obj, selector) {
+			var props = selector.split('.');
+			return (props.length === 1) ? obj[props[0]] : transverseByKey( obj[props.splice(0,1)], props.join('.') );
 		};
 
 		/*
@@ -42,6 +34,26 @@ steal(
 		 */
 		var groupIntoArray = function (collection, keys) {
 			var result = [];
+
+			var doIter = function (result, keys, value) {
+				// pop out first (destructive!)
+				var key = keys.splice(0,1);
+
+				// do nesting and finally push object
+				if (key.length > 0) {
+
+					// find or create nesting object
+					if (result.filter(function (item) { if (item['key'] === transverseByKey(value, key[0])) return item; }).length == 0) {
+						result.push({key: transverseByKey(value, key[0]), value: []});
+					}
+
+					// do iter
+					doIter(result[result.length-1].value, keys, value);
+					
+				} else {
+					result.push(value);
+				};
+			};
 			
 			$.each(collection, function (i, value) {
 
@@ -49,36 +61,13 @@ steal(
 				if (typeof keys === "string") { keys = [keys]; }
 				
 				// call recursion passing copy of the keys
-				groupIntoArrayIter(result, keys.slice(0), value);
+				doIter(result, keys.slice(0), value);
 				
 			});
 			return result;		
 		};
 
-		
-		var groupIntoObjectIter = function (result, keys, value) {
-			var key = keys.splice(0,1);
-
-			if (keys.length > 0) {
-
-				// if key doesn't exits create a new key/object pair 
-				if (!result[value[key]]) { result[value[key]] = {};	}
-
-				// do iter
-				groupIntoObjectIter(result[value[key]], keys, value);
 				
-			} else {
-				
-				// if key/array exists push value to it or create key/array pair.
-				if (result[value[key]]) {
-					result[value[key]].push(value);
-				} else {
-					result[value[key]] = [value];
-				}
-			}
-		};
-
-		
 		/*
 		 * Groups array recursively by keys into object
 		 *
@@ -97,13 +86,35 @@ steal(
 		var groupIntoObject = function (collection, keys) {
 			var result = {};
 
+			var doIter = function (result, keys, value) {
+				var key = keys.splice(0,1);
+
+				if (keys.length > 0) {
+
+					// if key doesn't exits create a new key/object pair 
+					if (!result[value[key]]) { result[value[key]] = {};	}
+
+					// do iter
+					doIter(result[value[key]], keys, value);
+					
+				} else {
+					
+					// if key/array exists push value to it or create key/array pair.
+					if (result[value[key]]) {
+						result[value[key]].push(value);
+					} else {
+						result[value[key]] = [value];
+					}
+				}
+			};
+
 			$.each(collection, function (i, value) {
 
 				// if value is string wrap it inside array
 				if (typeof keys === "string") { keys = [keys]; }
 				
 				// call recursion passoing copy of keys
-				groupIntoObjectIter(result, keys.slice(0), value);
+				doIter(result, keys.slice(0), value);
 				
 			});
 			return result;		
@@ -115,4 +126,3 @@ steal(
 		};
 		
 	});
-
