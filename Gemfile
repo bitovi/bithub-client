81cf99f9025fa968c3aa25ef6e38521029796b9b
Nikica Jokic <neektza@gmail.com>
2013-04-25 18:04:37 +0200

    Updated deploy script and Gemfile.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,5 @@
 source 'https://rubygems.org'
 
-gem 'foreman'
-
 group :development do
   gem 'capistrano'
   gem 'capistrano-ext'
--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set(:use_sudo, false)
 set(:ssh_options, { :forward_agent => true })
 set(:bundle_flags, "--deployment --quiet --binstubs")
 set(:scm, :git)
+set(:git_enable_submodules, 1)
 
 set(:user, "bithub")
 set(:application, "client")
