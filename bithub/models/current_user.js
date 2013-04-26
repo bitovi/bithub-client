6aa4a23d0cdb9ae19a08aadfb2df55ed7ae976db
Nikica Jokic <neektza@gmail.com>
2013-04-26 18:40:41 +0200

    New Post form submitting stuff.

--- bithub/models/current_user.js
+++ bithub/models/current_user.js
@@ -2,7 +2,8 @@ steal(
 		'can',
 		'bithub/models/user.js',
 		function (can, User) {
-			var current = new User({loggedIn: false});
+			// var current = new User({loggedIn: false});
+			var current = new User({loggedIn: true});
 			current.fromSession();
 			return current;
 		}
--- bithub/newpost/newpost.js
+++ bithub/newpost/newpost.js
@@ -1,6 +1,7 @@
 steal(
 	'can',
 	'./init.mustache',
+	'jquerypp/dom/form_params',
 	function(can, initView){
 		/**
 		 * @class bithub/newpost
@@ -17,6 +18,20 @@ steal(
 					this.element.html(initView({
 						projects: options.projects
 					}));
+					el.find("input[name='event[project]']").val(el.find('#newpost-form-project .btn.select').html());
+					el.find("input[name='event[category]']").val(el.find('#newpost-form-category .btn.select').html());
+				},
+
+				'#newpost-form-project a click': function( el, ev ) {
+					console.log(el.html());
+					el.closest('.btn-group').find('.btn.select').html(el.html());
+					el.closest('form').find("input[name='event[project]']").val(el.html());
+				},
+				
+				'#newpost-form-category a click': function( el, ev ) {
+					console.log(el.html());
+					el.closest('.btn-group').find('.btn.select').html(el.html());
+					el.closest('form').find("input[name='event[category]']").val(el.html());
 				},
 
 				'#hide-newpost-form-btn click': function( el, ev ) {
@@ -28,11 +43,18 @@ steal(
 					this.options.visibility() ? this.element.slideDown() : this.element.slideUp();
 				},
 
+				'button click': function( el, ev ) {
+					ev.preventDefault();
+				},
+
+				'#newpost-form-submit click': function( el, ev ) {
+					el.closest('form').submit();
+				},
+
 				'form submit': function( el, ev ) {
 					ev.preventDefault();
-
-					//
-					console.log("submitted");
+					var event = new Bithub.Models.Event(el.formParams());
+					event.save();
 				}
 				
 			});
