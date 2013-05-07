2dcb5888d908dd59c6a33f83fd2314563560dde0
Veljko Dragsic <veljko@kset.org>
2013-05-07 22:50:43 +0200

    LRU on smartselector

--- ui/smartselector/smartselector.js
+++ ui/smartselector/smartselector.js
@@ -1,22 +1,20 @@
 steal('can',
 	  './init.mustache',
-	  'bithub/assets/styles/app.css',
-	  'vendor/bootstrap',
+	  'jquerypp/dom/cookie',
 	  function(can, initView){
-		  /**
-		   * @class ui/smartselector
-		   * @alias Smartselector   
-		   */
+		  
+		  var items = new can.Observe.List();
+		  
+
 		  return can.Control(
 			  /** @Static */
 			  {
-				  defaults : {}
+				  defaults : { orderingCookieName: 'foobar' }
 			  },
 			  /** @Prototype */
 			  {
 				  init : function( elem, opts ){
-					  var self = this,
-						  items = opts.items;
+					  var self = this;
 					  
 					  self.element.html( initView( {
 						  htmlId: opts.htmlId,
@@ -27,9 +25,52 @@ steal('can',
 				  },
 
 				  'a.item click': function(el, ev) {
-					  this.options.state( can.data(el, 'item').name );
+					  var selected = can.data(el, 'item');
+					  this.options.state( selected.name );
 
+					  items.splice( items.indexOf(selected), 1 );
+					  items.unshift(selected);
+
+					  this.saveOrderingToCookie();
+					  
 					  ev.preventDefault();
+				  },
+
+				  '{items} change': function() {
+					  this.options.items.forEach( function( item ) {
+						  items.push({name: item.attr('name'), display_name: item.attr('display_name') });
+					  });
+					  this.loadOrderingFromCookie();
+				  },
+
+				  saveOrderingToCookie: function( cookieName ) {
+					  var cookie = cookieName || this.options.orderingCookieName,
+						  buff = [];
+
+					  items.forEach( function( item ) {
+						  buff.push( item.attr('name') );
+					  });
+					  $.cookie( this.options.orderingCookieName, buff.join(',') );
+				  },
+				  
+				  loadOrderingFromCookie: function( cookieName ) {
+					  cookieName = cookieName || this.options.orderingCookieName;
+
+					  if ( !$.cookie( cookieName ) ) return;
+
+					  // iter over item names from cookie
+					  can.each($.cookie( cookieName ).split(','), function( itemName ) {
+
+						  // match against items and reorder
+						  items.forEach( function( item, index ) {
+							  if ( item.attr('name') === itemName ) {
+								  items.splice( items.indexOf(item), 1 );
+								  items.push(item);
+								  return;
+							  }
+						  });
+					  });
+
 				  }
 
 			  });
