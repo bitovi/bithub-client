ee6ab723425970fe629f8a0e539d9c297e91a352
Veljko Dragsic <veljko@kset.org>
2013-05-20 12:59:25 +0200

    Leaderboard uses cached users list

--- bithub/leaderboard/leaderboard.js
+++ bithub/leaderboard/leaderboard.js
@@ -8,27 +8,48 @@ steal('can',
 		   */
 
 		  var defaultParams = {
-			  order: 'score:desc',
-			  limit: 6
+			  cached: true
+			  //order: 'score:desc',
+			  //limit: 6
 		  };
 
+		  var rank = can.compute( function( newVal ) {
+			  var current = 0;
+			  if (newVal) { current = newVal; }
+			  return current;
+		  });
+
+		  var breakAt = can.compute( function() {
+			  return (rank() > 7) ? rank()-1 : 0;
+		  });
+		  
+		  var topLength = can.compute( function() {
+			  if (rank() < 6) {
+				  return 6;
+			  } else if (rank() < 8) {
+				  return rank();
+			  } else {
+				  return 5;
+			  }
+		  });
+		  
 		  return can.Control(
 			  /** @Static */
 			  {
-				  defaults : {}
+				  defaults : {
+					  users: new Bithub.Models.User.List()
+				  }
 			  },
 			  /** @Prototype */
 			  {
-				  init : function() {
+				  init : function( elem, opts ) {
 					  var self = this;
 
-					  this.topUsers = new Bithub.Models.User.List();
-					  this.nearestUsers = new Bithub.Models.User.List();
-
 					  this.element.html( initView( {
-						  topUsers: self.topUsers,
-						  nearestUsers: self.nearestUsers,
-						  currentUser: self.options.currentUser
+						  users: this.options.users,
+						  currentUser: this.options.currentUser,
+						  breakAt: breakAt,
+						  topLength: topLength
 					  }, {
 						  isLoggedin: function (opts) {
 							  return (this.attr('loggedIn')) ? 'active' : '';
@@ -38,33 +59,31 @@ steal('can',
 					  this.updateLeaderboard();
 				  },
 
-				  // find the better way ...
 				  '{currentUser} change': function( user, ev, attr, how, newVal, oldVal ) {
-					  var self = this;
-
 					  if (attr === 'loggedIn' && newVal === true) {
-						  if ( user.attr('rank') > defaultParams.limit ) {
-							  User.findAll( can.extend({}, defaultParams, {offset: user.attr('rank') -3, limit: 6}), function( data ) {
-								  self.nearestUsers.replace( data );
-							  } );
-						  }
-
-						  /*
-						  this.topUsers.each(function (user) {
-							  if (self.options.currentUser.attr('id') === user.attr('id')) {
-								  user.attr('loggedIn', true);
-							  }
-						  });
-						   */
+						  this.determineRank();
 					  }
 				  },
-				  //
+
+				  '{users} change': function () {
+					  this.determineRank();
+				  },
+
+				  determineRank: function () {
+					  var self = this;
+					  
+					  this.options.users.each(function (user, i) {							  
+						  if (self.options.currentUser.attr('id') === user.attr('id')) {
+							  rank( i );
+						  }
+					  });
+				  },
 
 				  updateLeaderboard: function( params ) {
 					  var self = this;
 
 					  User.findAll( defaultParams, function ( data ) {
-						  self.topUsers.replace( data );
+						  self.options.users.replace( data );
 					  });
 				  }
 
