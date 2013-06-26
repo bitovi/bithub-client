a9648e24160301dfbff1239929b0439794e36bfc
Veljko Dragsic <veljko@proton>
2013-06-26 20:01:36 +0200

    Bootstrap modal with Twitter follow buttons is loaded when modal is shown.

--- bithub/modals/modals.js
+++ bithub/modals/modals.js
@@ -1,15 +1,16 @@
 steal('can',
 	  './init.mustache',
-	  function(can, initView) {
+	  './_twitter_modal.mustache',
+	  function(can, initView, twitterModal) {
 		  return can.Control(
 			  {
 				  defaults : {}
 			  }, {
 				  init : function(){
-					  this.element.html( initView({}) );
+					  this.element.html( initView({}) );					  
 				  },
-				  
-				  '#login-modal .providers .twitter a click': function( el, ev ) {
+
+				  '#login-modal .providers .twitter a click': function( el, ev ) {					  
 					  ev.preventDefault();
 					  el.closest('.modal').modal('hide');
 					  this.options.currentUser.login('twitter');
@@ -23,6 +24,12 @@ steal('can',
 
 				  showLogin: function () {
 					  this.element.find('#login-modal').modal('show');					  
+				  },
+
+				  showTwitter: function() {
+					  // twitter buttons are loaded dynamically
+					  $('#twitterModal').length || this.element.append( twitterModal({}) );
+					  $('#twitterModal').modal('show');
 				  }
 			  });
 	  });
