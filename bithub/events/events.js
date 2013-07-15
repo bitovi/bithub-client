fa7a9960b78f1e4718b8d29d34b4720cf7b74eea
Nikica Jokic <neektza@gmail.com>
2013-07-15 22:05:25 +0200

    Preserve event list height while waiting.
    ... for content. To prevent removing of scrollbar in firefox.

--- bithub/events/events.js
+++ bithub/events/events.js
@@ -230,12 +230,15 @@ steal('can',
 				  // spinner
 
 				  '{spinner} change': function( fn, ev, newVal, oldVal ) {
-					  var el = this.element;
+					  var el = this.element,
+						  preservedHeight = el.css('height'); // To prevent content bobbing (caused by removing of scrollbar in FF);
 					  
 					  if( newVal ) {
+						  el.css('height', preservedHeight);
 						  el.find('.events-container').hide();
 						  el.find('.spinner').show()
 					  } else {
+						  el.css('height', 'auto');
 						  el.find('.spinner').hide()
 						  el.find('.events-container').show();
 					  }
