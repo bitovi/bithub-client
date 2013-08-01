47d035659b9dc6bbc3076f2f86eaa5bcac11bf4b
Veljko Dragsic <veljko@proton>
2013-08-01 22:09:17 +0200

    EJS helper for making filter urls.

--- bithub/events/events.js
+++ bithub/events/events.js
@@ -80,8 +80,15 @@ steal('can',
 			}
 		}
 
-		var count = 0;
-		
+		can.EJS.Helpers.prototype.filterUrl = function ( item ) {			
+			return can.route.url( can.extend({}, {
+				category: can.route.attr('category') || 'all',
+				project: can.route.attr('project') || 'all',
+				view: can.route.attr('view') || 'all',
+				timespan: can.route.attr('timespan') || 'all'
+			}, item), false );
+		}
+
 		can.EJS.Helpers.prototype.renderEventTags = function (event) {
 			var buffer = "";
 			
