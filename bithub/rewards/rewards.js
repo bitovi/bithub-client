2f9c4aeef909e597acb68f9375e793066f6f1b52
Nikica Jokic <neektza@gmail.com>
2013-08-08 20:16:47 +0200

    Added 'reward' control.

---
+++ bithub/rewards/rewards.js
@@ -0,0 +1,18 @@
+steal(
+	'can',
+	'bithub/models/reward.js',
+	'./rewards.ejs',
+	function(can, Reward, rewardsView){
+		return can.Control({
+			defaults : {}
+		}, {
+			init : function (el, opts) {
+				Reward.findAll({}, function(rewards) {
+					el.html(rewardsView({
+						rewards: rewards
+					}));
+				});
+			}
+		});
+	}
+);
