fb9185322f4abfb152b9cea71097798843ec727c
Veljko Dragsic <veljko@kset.org>
2013-09-03 11:30:43 +0200

    Earn points page queries github for user watched/starred repos.

--- bithub/profile/earnpoints/earnpoints.js
+++ bithub/profile/earnpoints/earnpoints.js
@@ -2,18 +2,57 @@ steal('can',
 	  './init.mustache',
 	  '../_navbar.mustache',
 	  function(can, initView, navbarPartial){
+
+		  var watched = new can.Observe.List(),
+			  starred = new can.Observe.List();
+
 		  return can.Control.extend({
 				  defaults : {}
 			  }, {
-				  init : function(){
+				  init : function( elem, opts ){
+					  var self = this,
+						  user = opts.currentUser;
+					  
 					  this.element.html(initView({
-						  message: "Earn points"
+						  user: user
 					  }, {
-						  helpers: {},
+						  helpers: {
+							  hasProvider: function( provider, opts ) {
+								  return user.getProvider( provider ) ? opts.fn(this) : opts.inverse(this);
+							  },
+							  isStarred: function( repo, opts ) {
+								  starred.attr('length');
+								  return _.filter(starred, function( item ) {
+									  return item.attr('name') == repo;
+								  }).length ? opts.fn(this) : opts.inverse(this);
+							  },
+							  isWatched: function( repo, opts ) {
+								  watched.attr('length');
+								  return _.filter(watched, function( item ) {
+									  return item.attr('name') == repo;
+								  }).length ? opts.fn(this) : opts.inverse(this);
+							  }
+
+						  },
 						  partials: {
 							  navbarPartial: navbarPartial
 						  }
 					  }));
+
+					  if( user.isLoggedIn() ) this.loadGithub();
+				  },
+
+				  '{currentUser} isLoggedIn change': "loadGithub",
+
+				  loadGithub: function() {
+					  var user = this.options.currentUser;
+
+					  user.queryGithub("starred", function( data ) {
+						  starred.replace( data );
+					  });
+					  user.queryGithub("watched", function( data ) {
+						  watched.replace( data );
+					  });
 				  }
 			  });
 	  });
