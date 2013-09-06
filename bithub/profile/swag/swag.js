94fb087d384f24f057cf34cbd9d50ea64de77db6
Veljko Dragsic <veljko@kset.org>
2013-09-06 11:43:05 +0200

    Swag page renders achievements.

--- bithub/profile/swag/swag.js
+++ bithub/profile/swag/swag.js
@@ -1,14 +1,18 @@
 steal('can',
 	  './init.mustache',
 	  'bithub/models/reward.js',
+	  'vendor/moment',
 	  function(can, initView, Reward){
-		  
+
+
 		  return can.Control.extend({
-			  defaults : {}
+			  defaults : {
+				  rewards: new can.Observe.List()
+			  }
 		  }, {
 			  init : function( elem, opts ){
-				  var rewards = new can.Observe.List();
-
+				  var rewards = this.options.rewards;
+				  
 				  Reward.findAll({order: 'point_minimum'}, function( data ) {
 					  rewards.replace( data );
 				  });
@@ -17,6 +21,43 @@ steal('can',
 					  user: opts.currentUser,
 					  rewards: rewards
 				  }));
+
+				  if( opts.currentUser.isLoggedIn() ) {
+					  this.matchRewards();
+				  }
+			  },
+
+			  '{currentUser} isLoggedIn' : "matchRewards",
+			  '{rewards} length': "matchRewards",
+
+			  matchRewards: function() {
+				  var user = this.options.currentUser,
+					  rewards = this.options.rewards;
+
+				  _.each(user.attr('achievements'), function( achievement ) {
+					  var status = "";
+					  
+					  if( achievement.attr('achieved_at') ) {
+						  status = {
+							  cssClass: "shipping",
+							  message: "This is a great thing!"
+						  }
+					  }
+					  if( achievement.attr('shipped_at') ) {
+						  status = {
+							  cssClass: "achieved",
+							  inlineMessage: "Shipped " + moment( achievement.attr('shipped_at') ).format('MM/DD/YY')
+						  }
+					  }
+					  
+					  _.each(rewards, function( reward ) {
+						  if( achievement.attr('reward_id') == reward.attr('id') ) {
+							  reward.attr('status', status );
+						  }
+						  
+					  });
+				  });
 			  }
+
 		  });
 	  });
