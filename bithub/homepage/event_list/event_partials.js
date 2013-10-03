90d2ad826e051cf93deaa9b0c6eee8b03a1e461d
Veljko Dragsic <veljko@kset.org>
2013-10-03 19:08:15 +0200

    One more '_' added to event subpartial filenames.

--- bithub/homepage/event_list/event_partials.js
+++ bithub/homepage/event_list/event_partials.js
@@ -4,10 +4,10 @@ steal(
 	'bithub/homepage/event_list/views/_event_child_event.ejs',
 	'bithub/homepage/event_list/views/_digest.ejs',
 	'bithub/homepage/event_list/views/_code.ejs',
-	'bithub/homepage/event_list/views/_status_bar.ejs',
-	'bithub/homepage/event_list/views/_manage_bar.ejs',
-	'bithub/homepage/event_list/views/_toolbar.ejs',
-	'bithub/homepage/event_list/views/_upvote.ejs',
+	'bithub/homepage/event_list/views/__status_bar.ejs',
+	'bithub/homepage/event_list/views/__manage_bar.ejs',
+	'bithub/homepage/event_list/views/__toolbar.ejs',
+	'bithub/homepage/event_list/views/__upvote.ejs',
 	'bithub/homepage/event_list/determine_event_partial.js',
 	function(can, eventChildrenPartial, eventChildEventPartial, digestPartial, codePartial, statusBarPartial, manageBarPartial, toolbarPartial, upvotePartial, determineEventPartial) {
 
