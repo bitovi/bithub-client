a8e482ff6fd3b0dbc169f865394a8d7259ad738a
Veljko Dragsic <veljko@kset.org>
2013-10-08 16:02:50 +0200

    documentjs added to routes.

--- bithub/bithub.js
+++ bithub/bithub.js
@@ -34,7 +34,7 @@ steal(
 		
 		// Routes
 
-		var projects = ['canjs', 'donejs', 'javascriptmvc', 'funcunit', 'jquerypp', 'stealjs', 'canui'],
+		var projects = ['canjs', 'donejs', 'javascriptmvc', 'funcunit', 'jquerypp', 'stealjs', 'canui', 'documentjs'],
 			categories = ['bug', 'issue', 'twitter', 'question', 'article', 'comment', 'app', 'code', 'chat', 'plugin', 'event', 'feature'],
 			timespans = ['day', 'week', 'month', 'all'];
 
