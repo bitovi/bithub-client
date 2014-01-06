d3bc9b9ebc9d2276794387c8133b843c8d2fb2db
Mihael Konjevic <konjevic@gmail.com>
2014-01-06 18:01:23 +0100

    Use Event.model instead of attr to wrap data from the upload

--- bithub/postform/postform.js
+++ bithub/postform/postform.js
@@ -293,7 +293,7 @@ function(Component, postformView, EventModel, TagModel, PostAsUserModel){
 			eventSaved : function(event){
 				if(!(event instanceof EventModel)){
 					console.log('EVENT FROM THE UPLOAD', event)
-					this.scope.attr('event', event);
+					EventModel.model(event);
 				}
 
 				if(this.scope.__oldEvent){
