bf89ac1306243812045226dc4fa687ae680e88f8
Mihael Konjevic <konjevic@gmail.com>
2014-02-07 18:11:05 +0100

    Implement meetup and rsvps

--- bithub/entities/event/event.js
+++ bithub/entities/event/event.js
@@ -5,11 +5,28 @@ steal(
 'bithub/entities/shared_helpers.js',
 'bithub/entities/events.js',
 function(Component, eventView, EntityState, sharedHelpers, sharedEvents){
+
+
 	can.Component.extend({
 		tag : 'bh-event',
 		template : eventView,
 		scope : EntityState.extend({
-			inited : true
+			inited : true,
+			sortedChildren : function(){
+				var children = this.attr('event.children');
+				var sorted =  [].sort.call(children, function(a, b){
+
+					var sortA = a.attr('props.response'),
+						sortB = b.attr('props.response');
+					if(sortA > sortB){
+						return -1;
+					} else if(sortB > sortA){
+						return 1;
+					}
+					return 0;
+				})
+				return sorted
+			}
 		}),
 		helpers : can.extend({}, sharedHelpers),
 		events : can.extend({}, sharedEvents)
