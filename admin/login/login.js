aabfa9330543d734cb737b6f5ec141164aa8f81a
Mihael Konjevic <konjevic@gmail.com>
2014-04-25 17:52:07 +0200

    new oauth url

--- admin/login/login.js
+++ admin/login/login.js
@@ -7,7 +7,14 @@ steal('can/util/string', 'can/compute',
 			connect: function(options) {
 				var windowPropsStr = options.windowPropsStr ? options.windowPropsStr : "width=600,height=300,scrollbars=yes";
 				var title = options.title ? options.title : "Oauth login";
-				var url = '/api/auth/' + options.feed + '_brand';
+				var host = window.location.host.split('.');
+				var url = can.sub('http://{host}/api/login_and_oauth?oauth_acc={acc}&brand={brand}', {
+					host: host.splice(1, host.length - 1).join('.'),
+					brand: options.feed,
+					acc:BRAND.attr('acc')
+				})
+
+				console.log(url)
 
 				CONNECTING_FEED(options.feed);
 
